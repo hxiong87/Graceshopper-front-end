79c3f9b3326a3d2abac4f65748c6706e3e5f6599
Joel Donkoh <sethjoel.sethdonkoh@gmail.com>
2023-04-24 20:00:38 -0400

    update to Admin Branch

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -40,10 +40,12 @@ async function fetchAllUsers() {
   }
 }
 
-async function updateUser({ userId, obj, token }) {
+async function updateUser( userId, obj, token ) {
+  console.log ('userId', userId, obj, token);
+  
 
   try {
-      const response = await fetch(`https://graceshopper-0xzy.onrender.com/api/${userId}`, {
+      const response = await fetch(`https://graceshopper-0xzy.onrender.com/api/users/${userId}`, {
         method: "PATCH",
         headers: {
           'Content-Type': 'application/json',
@@ -55,6 +57,7 @@ async function updateUser({ userId, obj, token }) {
       if (result.error) {
           throw result.error;
       }
+      console.log(result);
       return result;
     } catch (error) {
       console.error(error);
@@ -72,6 +75,8 @@ export const Admin = () => {
   const [userId, setUserId] = useState();
   const [isEngineer, setIsEngineer] = useState();
   const [isAdmin, setIsAdmin] = useState();
+  const [admin, setAdmin] = useState();
+  const [engineer, setEngineer] = useState();
   const token = window.localStorage.getItem('token')
   const handleSubmit = async event => {
       event.preventDefault();
@@ -89,14 +94,30 @@ export const Admin = () => {
     fetchData();
   })
 
+  // if (admin == 'on')  {
+  //   setIsAdmin(true)
+  // } else {
+  //   setIsAdmin(false)
+  // }
+
+  // if (engineer == 'on') {
+  //   setIsEngineer(true)
+  // } else {
+  //   setIsEngineer(false)
+  // }
+
+
+
+
   const handleUser = async (event) => {
     event.preventDefault();
     console.log("event target", event.target[3].value)
     setUserId(event.target[3].value);
     console.log('userId', userId);
-    const obj = { isEngineer, isAdmin };
+    const obj = { id: userId, isEngineer, isAdmin };
     console.log("updateUser", userId, obj, token)
-    await updateUser(userId, obj, token);
+    const updated = await updateUser(userId, obj, token);
+    return updated;
   }
 
   return (
@@ -157,20 +178,20 @@ export const Admin = () => {
                 Email {user.email}
               </h4>
               <div>
-                Engineer {user.engineer ? `Yes` : `No`}
+                Engineer {user.engineer ? "Yes" : "No" } 
               </div>
               <div>
-                Admin {user.admin ? `Yes` : `No`}                  
+                Admin {user.admin ? "Yes" : "No"}                  
               </div>
               <form onSubmit={handleUser}>
                 <div>
                   <label>
                     <p>Engineer</p>
-                    <input type='checkbox' onChange={event => setIsEngineer(event.target.value)}/>
+                    <input type='checkbox' onChange={event => setIsEngineer(true)}/>
                   </label>
                   <label>
                     <p>Admin</p>
-                    <input type='checkbox' onChange={event => setIsAdmin(event.target.value)}/>
+                    <input type='checkbox' onChange={event => setIsAdmin(true)}/>
                   </label>
                 </div>
                 <button type='submit'>Click TWICE to Submit</button>
