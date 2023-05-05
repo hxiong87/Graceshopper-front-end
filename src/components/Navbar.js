32f150ed0b94aaffc72c7a4a92c3fad6c2b74c62
hxiong87 <117489323+hxiong87@users.noreply.github.com>
2023-05-04 21:35:16 -0500

    css updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,34 +29,6 @@ export const Navbar = ({ isLoggedIn, logout, adminPrivileges }) => {
           <img className="nav-img" src="./sfp.png" alt="home backgroud img" />
         </div>
 
-        {/* <nav className="nav-main">
-       
-            <div className='navigation-links'>
-                <ul className="leftside-link">
-                    <Link className='home' to='/'>Home</Link>
-                    <Link className="products" to='/products'>Products</Link>
-                    <Link className="profile" to='/profile'>Profile</Link>
-                    {isLoggedIn && <Link className="admin-tab" to='/admin'>Admin</Link>}
-                </ul>  
-                </div>
-                <div className="nav-logo">
-                <img className="nav-img" src='./sfp.png' alt="home backgroud img" />
-                </div>
-               
-                <div className="navigation-links2">
-                <ul className="rightside-links">
-                     <Link className='login' to='/login'>Login</Link>
-                    <Link className='register' to='/register'>Register</Link>
-                    <Link className="cart" to='/Cart'>Shopping Cart</Link>
-                   {isLoggedIn && <Link to='/' onClick={() => logout()}>Logout</Link>}
-                </ul>
-               
-              
-               
-            </div>
-   
-         </nav> */}
-
         <div className="navigation-links2">
           <ul className="rightside-links">
             <Link className="login" to="/login">
@@ -66,7 +38,7 @@ export const Navbar = ({ isLoggedIn, logout, adminPrivileges }) => {
               Register
             </Link>
             <Link className="cart" to="/Cart">
-              Shopping Cart
+               Cart
             </Link>
             <Link to="/" onClick={() => logout()}>
               Logout
